06209fb523f623db3af70e3d8cd8653ca9ec7a24
BorjaSanchidrian <b.sanchidrianmonge@gmail.com>
2014-07-13 22:34:40 +0200

    update

--- BorjaScript.user.js
+++ BorjaScript.user.js
@@ -2,49 +2,49 @@
 // @name        BorjaScript
 // @namespace   http://spain-estudios.com/
 // @include     http://spain-estudios.com/?survey=job
-// @version     1.2
+// @include     http://www.spain-estudios.com/?survey=job
+// @version     1.1
 // @grant       none
 // @description Spain-estudios script por Borja
 // ==/UserScript==
 
-// -- [VARIABLES] --
-	//No tocar a menos que sepas lo que haces.
+var surv = $('.surv').html();
+var surv_num = surv.replace(/\D+/g, '');
 
-	var surv 		 = $('.surv').html();		  //Título de la pregunta
-	var surv_num 	 = surv.replace(/\D+/g, '');  //Quita las letras del título dejando solo los números
-	var answer_click = false;					  //Detecta cuando se ha pulsado el botón "Responder".
-	window.onload 	 = scriptTime;				 
+var num_name = surv_num;
+if(num_name == '410') { num_name = '4'; } //Bug con la pregunta 4
 
-	
-// -- [EXTRA] --
-	if(surv_num == '410') {surv_num = '4';} //Bug con la pregunta 4
+var timerVar = setInterval(function () {
+    Script();
+}, 1000);
 
-// -- [FUNCIONES] --
-	function scriptTime() {
-		//Cada cuanto se repite la función Script (1000ms)
-		if(!answer_click) {
-			setInterval(function() { Script(); }, 1000);
-		}
-	}
+function Script() {
+    var done = 0;
+    
+    if ($('[name="q' + num_name + '"]').length > 0) {
+	    
+	    //usar con precaucion
+	    //$("#TimerButton").prop('value', 'Responder');
+	    //$("#TimerButton").prop('disabled', false);
 
-	function Script() {
-	   
-	    if ($('[name="q' + surv_num + '"]').length > 0) {
 
-	        if ($('[name="q' + surv_num + '"]').is(':checked')) {
-	            //hay una opcion selecionado
-	            if ($('#TimerButton').prop('value') == 'Responder') {
-	                $('#TimerButton').click();
-	                answer_click = true;
-	            }
-	        } else {
-	            //si no hay una opcion selecionada
-	            var value = 1 + Math.floor(Math.random() * 3);
-	            $('[value^=\'' + value + '\']').prop('checked', true)
-	            if ($('#TimerButton').prop('value') == 'Responder') {
-	                $('#TimerButton').click();
-	                answer_click = true;
-	            }
-	        }
-	    }
-	}
+        if ($('[name="q' + num_name + '"]').is(':checked')) {
+            //hay una opcion selecionado
+            if ($('#TimerButton').prop('value') == 'Responder') {
+                $('#TimerButton').click();
+                done = 1;
+            }
+        } else {
+            //si no hay una opcion selecionada
+            var value = 1 + Math.floor(Math.random() * 3);
+            $('[value^=\'' + value + '\']').prop('checked', true)
+            if ($('#TimerButton').prop('value') == 'Responder') {
+                $('#TimerButton').click();
+                done = 1;
+            }
+        }
+        if (done == 1) {
+          	setTimeout(function () { }, 7000);
+        }
+    }
+}
